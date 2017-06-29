b9220d1dc221d0d9d384c18b120f9456ca8e4502
Paul Young <yyang173@illinois.edu>
2017-06-28 17:40:52 -0700

    build multidet wavefunction file

--- library/pyscf_orbital_routines.py
+++ library/pyscf_orbital_routines.py
@@ -78,20 +78,15 @@ def save_eigensystem(mf,gvec_fname = 'gvectors.dat'
     ispin = 0 # restricted (same orbitals for up and down electrons)
     # get MOs in plane-wave basis
     aoR = ao_on_grid(mf.cell)
-    # sort MOs
-    idx = np.argsort(mf.mo_energy)
-    if not np.allclose(idx,np.arange(len(mf.mo_energy))):
-      raise NotImplementedError('mo_coeff is not sorted')
-    # end if
-    gvecs,psig = mo_coeff_to_psig(mf.mo_coeff,aoR,mf.cell.gs,mf.cell.vol)
-    nstate,npw,ncomp = psig.shape
-    for istate in range(nstate):
-      entry = {'ikpt':ikpt,'ispin':ispin,'istate':istate,
-        'reduced_k':mf.kpt,'evalue':mf.mo_energy[istate],'evector':psig[istate,:,:]}
-      data.append(entry)
-    # end for istate
-    eig_df = pd.DataFrame(data).set_index(
-      ['ikpt','ispin','istate'],drop=True).sort_index()
+    gvecs,eig_df = mo_orbitals(mf.mo_coeff,aoR,mf.cell.gs,mf.cell.vol)
+    eig_df['ispin'] = ikpt
+    eig_df['ikpt']  = ispin
+    kpt_data = np.zeros([len(eig_df),len(mf.kpt)])
+    kpt_data[:] = mf.kpt # copy kpt to every row of kpt_data
+    eig_df['reduced_k'] = kpt_data.tolist()
+    eig_df.set_index(['ikpt','ispin','istate'],inplace=True,
+      drop=True)
+    eig_df.sort_index()
     if save:
       eig_df.reset_index().to_json(eigsys_fname)
       np.savetxt(gvec_fname,gvecs)
@@ -99,6 +94,82 @@ def save_eigensystem(mf,gvec_fname = 'gvectors.dat'
   # end if
   return gvecs,eig_df
 # end def save_eigensystem
+
+def mo_orbitals(mo_energy,mo_coeff,ao_on_grid,grid_shape,cell_volume):
+  """ save molecular orbitals (MOs) in plane-wave basis in a database 
+  Input:
+    mo_energy: MO eigenvalues, written to database but only used to sort the MOs
+    mo_coeff: MO eigenvectors in atomic orbital (AO) basis
+    ao_on_grid: AOs on a real-space grid
+    grid_shape: shape of the real-space grid 
+    cell_volume: volume of the simulation cell, only used to normalized Fourier transform
+  Output:
+    gvecs: a list of integer vectors of shape (npw,ndim), which represent the plane-wave basis in reciprocal space units
+    eig_df: a pandas dataframe, one entry for each MO
+  """
+  import pandas as pd
+
+  # perform Fourier transform to get plane-wave coefficients psig in basis gvecs
+  gvecs,psig = mo_coeff_to_psig(mo_coeff,ao_on_grid,grid_shape,cell_volume)
+
+  # build dataframe using sorted MOs
+  sorted_state_indices = np.argsort(mo_energy)
+  istate = 0
+  data = []
+  for idx in sorted_state_indices:
+    entry = {'istate':istate,'evalue':mo_energy[idx],'evector':psig[idx,:,:]}
+    data.append(entry)
+    istate += 1
+  # end for
+  df = pd.DataFrame(data)
+  return gvecs,df
+# end def mo_orbitals
+
+def multideterminant_coefficients(detlist,nfill,mo_coeff):
+  """ save all mo_coefficients needed by a multideterminant expansion
+  Inputs:
+    detlist: list of determinants in MO basis, shape (ndet,nmo*nmo)
+    nfill: number of filled orbitals for each determinant, integer 
+    mo_coeff: MOs in AO basis, shape (nao,nmo)
+  Outputs:
+    new_mo_coeff: determinant orbitals in AO basis, shape (nao,ndet*nfill)
+  """
+  ndet,nb = detlist.shape
+  nao,nmo = mo_coeff.shape
+  assert nb == nao*nmo
+  
+  new_mo_coeff = np.zeros([nao,ndet*nfill],dtype=complex)
+
+  for idet in range(ndet):
+    for iorb in range(nfill):
+      det = detlist[idet].reshape(nmo,nmo)
+      new_mo_coeff[:,idet*nfill:idet*nfill+nfill] = np.dot(mo_coeff,det)[:,:nfill]
+    # end for iorb
+  # end for idet
+  return new_mo_coeff
+# end def multideterminant_coefficients
+
+def save_multideterminant_orbitals(detlist,nfill,mf):
+  ikpt = 0
+  ispin= 0
+  aoR = ao_on_grid(mf.cell)
+  new_mo_coeff = multideterminant_coefficients(detlist,nfill,mf.mo_coeff)
+  norb = new_mo_coeff.shape[1]
+  fake_mo_energy = np.arange(norb)
+
+  gvecs,eig_df = mo_orbitals(fake_mo_energy,new_mo_coeff,aoR,mf.cell.gs,mf.cell.vol)
+
+  # finish dataframe
+  eig_df['ikpt']  = ikpt
+  eig_df['ispin'] = ispin
+  eig_df.to_json('test.json')
+  kpt_data = np.zeros([len(eig_df),len(mf.kpt)])
+  kpt_data[:] = mf.kpt # copy kpt to every row of kpt_data
+  eig_df['reduced_k'] = kpt_data.tolist()
+  eig_df.set_index(['ikpt','ispin','istate'],inplace=True,drop=True)
+  eig_df.sort_index()
+  return gvecs,eig_df
+# end def save_multideterminant_orbitals
 
 def generate_pwscf_h5(cell,gvecs,eig_df,pseudized_charge=None,h5_fname='pyscf2pwscf.h5'):
   
